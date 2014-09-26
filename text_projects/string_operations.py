bd3e08f76f16f6d90987b87bb7f36b2b480e9d56
Vivek Poddar <vpoddar@google.com>
2014-09-26 08:36:08 +0530

    Fix.

--- text_projects/string_operations.py
+++ text_projects/string_operations.py
@@ -7,52 +7,52 @@ Created on 09/18/2014
 class MyString(object):
     """String manipulations"""
 
-    def reverse(self, aString):
+    def reverse(self, input_string):
         """
         Parameters:
-            aString: The string to reverse.
+            input_string: The string to reverse.
         Returns:
             Reversed string.
         """
         # list[start::end::step]
-        return "".join(list(aString)[::-1])
+        return "".join(list(input_string)[::-1])
 
-    def count_vowels(self, aString):
+    def count_vowels(self, input_string):
         """
         Parameters:
-            aString: The string to count vowels in.
+            input_string: The string to count vowels in.
         Returns:
             Map {vowel: count}.
         """
         vowels = ['a', 'e', 'i', 'o', 'u']
         res = {char: 0 for char in vowels}
-        for each in list(aString):
+        for each in list(input_string):
             if each in vowels:
                 res[each]+= 1
         return res
 
-    def count_words(self, aString):
+    def count_words(self, input_string):
         """
         Parameters:
-            aString: The string to count words in.
+            input_string: The string to count words in.
         Returns:
             The number of words.
         """
-        return len(aString.split())
+        return len(input_string.split())
 
-    def is_palindrome(self, aString):
+    def is_palindrome(self, input_string):
         """
         Parameters:
-            aString: The input string.
+            input_string: The input string.
         Returns:
             Whether the string is palindrome or not.
         """
-        return self.reverse(aString) == aString
+        return self.reverse(input_string) == input_string
 
-    def encrypt(self, aString, shift=0):
+    def encrypt(self, input_string, shift=0):
         """
         Parameters:
-            aString: The string to encrypt.
+            input_string: The string to encrypt.
             [a, b, c] = [0, 1, 2]
         Returns:
             The cipher text.
@@ -67,7 +67,7 @@ class MyString(object):
             return code
 
         res = ''
-        for char in aString:
+        for char in input_string:
             if char.isalpha():
                 code = get_encrypt_code(char)
             elif ord(char) == 32: # space character
@@ -78,9 +78,9 @@ class MyString(object):
 
 if __name__ == '__main__':
     myString = MyString()
-    aString = 'Foo Bar'
-    print myString.reverse(aString)
-    print myString.count_vowels(aString)
-    print myString.count_words(aString)
+    input_string = 'Foo Bar'
+    print myString.reverse(input_string)
+    print myString.count_vowels(input_string)
+    print myString.count_words(input_string)
     print myString.is_palindrome('MADAM')
     print myString.encrypt('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD', 3)
